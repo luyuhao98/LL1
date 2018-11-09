52760d74734c48e7d9b7cd617ae358dea67c16b2
Luyuhao <Luyuhao98@icloud.com>
2018-11-09 11:12:35 +0800

    eliminate left recursion & first

--- LL1.cpp
+++ LL1.cpp
@@ -4,10 +4,10 @@
 #include <set>
 #include <fstream>
 #include <map>
-
-#include <algorithm>
-#include <typeinfo>
 #include <iomanip>
+
+// #include <algorithm>
+// #include <typeinfo>
 
 using namespace std;
 
@@ -16,24 +16,28 @@ using namespace std;
 /*文件读入流*/
 ifstream fin("input.txt");
 
-/*文件输出流*/
-ofstream fout("output.txt");
+// /*文件输出流*/
+// ofstream fout("output.txt");
 
 /*非终结符对应项目*/
 class item
 {
+      public:
         set<string> first;  //first set
         set<string> follow; //follow set
         /*产生式集合,每个产生式是string的集合*/
         vector<vector<string>> prods;
 };
 
-/*非终结符集合*/
+/*原非终结符集合*/
 
-map<string, item> N;
+map<string, item> Nin;
+
+/*修改后终结符集合*/
+map<string, item> Nmodified;
 
 /*终结符集合*/
-set<stirng> T;
+set<string> T;
 
 /*-------函数区----------*/
 
@@ -41,10 +45,10 @@ set<stirng> T;
 /*按行读取，跳过空行，跳过空格，词法分析，拆分多句*/
 void read()
 {
-        string input;    //每行输入
-        int i_input = 0; //input下标
-        string str;      //待存符号
-        char in;         //带判断字符
+        string input; //每行输入
+        string str;   //待存符号
+        int i_input;  //input下标
+        char in;      //带判断字符
 
         int row = 0;
         while (getline(fin, input))
@@ -56,12 +60,12 @@ void read()
                 /*---词法分析部---*/
 
                 //1.获取产生式左部，限制必须为非终结符，即必须为大写字母。设置标记
-                state = 0; //未获取产生式左部状态
+                int state = 0; //未获取产生式左部状态
                 string left;
                 item right;
-                vector<input> prod; //产生式
+                vector<string> prod; //产生式
 
-                for (; i_input < input.size(); i_input++)
+                for (i_input = 0; i_input < input.size(); i_input++)
                 {
                         //        str.clear();
                         in = input[i_input];
@@ -79,6 +83,7 @@ void read()
                                 }
                                 if (state == 2 && str.size())
                                 {
+                                        T.insert(str);
                                         prod.push_back(str);
                                         str.clear();
                                 }
@@ -110,7 +115,7 @@ void read()
                                 {
 
                                         str.clear();
-                                        state == 2;
+                                        state = 2;
                                 }
                                 else if (str.size() == 1 && !str.compare("-"))
                                 {
@@ -133,7 +138,7 @@ void read()
                                 {
                                         continue;
                                 }
-                                //单符号，如‘A’终结符 ，如‘*’ 非终结符，以及‘|’分界符`
+                                //单符号，如‘A’终结符 ，如‘*’ 非终结符，如‘|’分界符`,以及空字符表示’？‘
                                 else
                                 {
                                         //如果只有单字符，str长度为1
@@ -144,6 +149,7 @@ void read()
 
                                                 prod.push_back(str);
                                                 str.clear();
+                                                T.insert(str);
 
                                                 str.push_back(in); //收回单符号
                                         }
@@ -157,6 +163,8 @@ void read()
                                         }
                                         else
                                         {
+                                                if (!((in >= 'A' && in <= 'Z') || in == '?'))
+                                                        T.insert(str);
                                                 prod.push_back(str);
                                                 str.clear();
                                         }
@@ -170,6 +178,7 @@ void read()
                         //如果符号串不为空，将str结尾存入产生式prod
                         if (str.size())
                         {
+                                T.insert(str);
                                 prod.push_back(str);
                                 str.clear();
                         }
@@ -180,9 +189,9 @@ void read()
                                 right.prods.push_back(prod);
                                 prod.clear();
                         }
-                        
+
                         //将左右部分压入非终结符map
-                        N.insert(left, right);
+                        Nin.insert(pair<string, item>(left, right));
                         left.clear();
                         right.prods.clear();
                 }
@@ -193,7 +202,190 @@ void read()
                 }
         }
 }
+//输出产生式
+void outProds(map<string, item> &N)
+{
+        map<string, item>::iterator iter = N.begin();
+        while (iter != N.end())
+        {
+                vector<vector<string>>::iterator it1 = iter->second.prods.begin();
+                while (it1 != iter->second.prods.end())
+                {
+                        cout << iter->first << "->";
+                        vector<string>::iterator it2 = (*it1).begin();
+                        while (it2 != (*it1).end())
+                        {
+                                cout << *it2;
+                                it2++;
+                        }
+                        cout << endl;
+                        it1++;
+                }
+                iter++;
+        }
+}
 
-int main(){
-        
+
+void outset(){
+        map<string, item>::iterator iter = Nmodified.begin();
+        while (iter != Nmodified.end())
+        {       
+                /*输出First集*/
+                cout<<"First set of < "<<iter->first<<" > : { ";
+                set<string>::iterator it = iter->second.first.begin();
+                while (it != iter->second.first.end())
+                {
+                        cout<<*it<<" ";
+                        it++;
+                }
+                cout<<"}"<<endl;
+
+                /*输出FOLLOW集*/
+                cout<<"Follow set of < "<<iter->first<<" > : { ";
+                it = iter->second.follow.begin();
+                while (it != iter->second.follow.end())
+                {
+                        cout<<*it<<" ";
+                        it++;
+                }
+                cout<<"}"<<endl;
+
+                iter++;
+        }
+}
+//输出终结符
+void outT()
+{
+        set<string>::iterator it = T.begin();
+        cout << "----T----" << endl;
+
+        while (it != T.end())
+        {
+                cout << *it << endl;
+                it++;
+        }
+}
+
+//消除左递归
+void elimimateleftrecursion()
+{
+        map<string, item>::iterator iter = Nin.begin();
+        while (iter != Nin.end())
+        {
+                vector<vector<string>>::iterator it = iter->second.prods.begin();
+                int needEliminate = 0;
+                vector<int> recurse;
+
+                //定义Nmodified的左右
+                while (it != iter->second.prods.end())
+                {
+                        if (!iter->first.compare((*it)[0]))
+                        {
+                                needEliminate = 1;
+                                recurse.push_back(1);
+                        }
+                        else
+                        {
+                                recurse.push_back(0);
+                        }
+                        it++;
+                }
+
+                /*---Nmodified---*/
+                //如果需要修改
+                if (needEliminate)
+                {
+                        string left1(iter->first);
+                        string left2(iter->first);
+                        left2.push_back('\'');
+                        item right1, right2;
+
+                        for (int i = 0; i < recurse.size(); i++)
+                        {
+                                if (recurse[i])
+                                {
+                                        vector<string> prod;
+                                        prod.assign(iter->second.prods[i].begin() + 1, iter->second.prods[i].end());
+                                        prod.push_back(left2);
+                                        right2.prods.push_back(prod);
+                                }
+                                else
+                                {
+                                        vector<string> prod(iter->second.prods[i]);
+                                        prod.push_back(left2);
+                                        right1.prods.push_back(prod);
+                                }
+                        }
+                        Nmodified.insert(pair<string, item>(left1, right1));
+                        Nmodified.insert(pair<string, item>(left2, right2));
+                }
+                else
+                {
+                        Nmodified.insert(*iter);
+                }
+
+                needEliminate = 0;
+                recurse.clear();
+                iter++;
+        }
+}
+
+set<string> findfirst(string left)
+{
+        //return set
+        set<string> myset;
+
+        //终结符或者空
+        if (T.count(left) || !left.compare("?"))
+        {
+                myset.insert(left);
+        }
+        else
+        {
+                //非终结符
+                item right(Nmodified[left]);
+                //it1 产生式指针
+                vector<vector<string>>::iterator it1 = right.prods.begin();
+                while (it1 != right.prods.end())
+                {
+                        //it2 产生式内元素指针
+                        vector<string>::iterator it2 = it1->begin();
+
+                        while (it2 != it1->end())
+                        {
+                                set<string> get = findfirst(*it2);
+                                myset.insert(get.begin(), get.end());
+
+                                if (get.count("?") && it2 != it1->end() - 1)
+                                {
+                                        myset.erase("?");
+                                        continue;
+                                }
+                                break;
+                                it2++;
+                        }
+                        it1++;
+                }
+        }
+        return myset;
+}
+void fisrt()
+{
+        map<string, item>::iterator iter = Nmodified.begin();
+        while (iter != Nmodified.end())
+        {
+                set<string> get = findfirst(iter->first);
+                iter->second.first.insert(get.begin(),get.end());
+                iter++;
+        }
+}
+
+int main()
+{
+        read();
+        elimimateleftrecursion();
+        outProds(Nmodified);
+        fisrt();
+        outset();
+        outT();
 }
